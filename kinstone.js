f6c9b8f697f8150ea42b880f6d3ba8f134a320e8
johnpatrickanders <johnpatrickanders@gmail.com>
2021-02-18 15:11:26 -0500

    configure env for ks

--- kinstone.js
+++ kinstone.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -6,8 +8,8 @@ const puppeteer = require('puppeteer');
 
   await page.goto('https://www.google.com/admin');
 
-  await page.type('#username', 'foo@example.com');
-  await page.type('#password', 'password');
+  await page.type('#username', process.env.SENDER_EMAIL);
+  await page.type('#password', process.env.KS_PASSWORD);
   await page.keyboard.press('Enter');
 
   await page.waitForNavigation();
